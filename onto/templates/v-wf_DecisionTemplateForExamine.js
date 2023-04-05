dc4b13870fe34251c864df7af1025858e1f89111
lav <lapshin.a.v.395@gmail.com>
2023-04-05 17:09:18 +0300

    feat(ui): add template for examine without comment

---
+++ onto/templates/v-wf_DecisionTemplateForExamine.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+
+export const pre = function (individual, template, container, mode, extra) {
+  template = $(template);
+  container = $(container);
+
+  template.on('cancel', function () {
+    const modal = template.closest('.modal');
+    if (modal.length) {
+      modal.modal('hide');
+    } else {
+      window.history.back();
+    }
+  });
+};
+
+export const html = `
+  <div>
+    <div class="view -edit -search">
+      <span rel="v-s:creator">
+        <span>
+          <strong rel="v-s:employee" data-template="v-ui:LabelTemplate"></strong>
+          <small rel="v-s:occupation" data-template="v-ui:LabelTemplate"></small>
+        </span>
+      </span>
+      <small>
+        <span>&bullet;&nbsp;&nbsp;</span>
+        <span property="v-s:created"></span>
+      </small>
+      <hr class="margin-sm" />
+    </div>
+    <h4 class="decision" about="@" property="rdfs:label"></h4>
+    <div class="actions -view edit -search">
+      <span about="@" data-template="v-ui:StandardButtonsTemplate" data-embedded="true" data-buttons="send cancel"></span>
+    </div>
+  </div>
+`;
