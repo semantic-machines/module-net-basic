4d16fbf32b806a898a1b08f07bca6a2c13e34d08
lenkovets <lenkovez.as@gmail.com>
2025-02-05 16:35:00 +0300

    Update s-wf_SimpleRouteStartFormShort_Template.js

--- onto/templates/s-wf_SimpleRouteStartFormShort_Template.js
+++ onto/templates/s-wf_SimpleRouteStartFormShort_Template.js
@@ -1,14 +1,60 @@
 export const pre = function (individual, template, container, mode, extra) {
-  if (individual.tooltip != undefined) {
+  template = $(template);
+  container = $(container);
+  if (individual.hasValue('s-wf:SimpleRouteStartForm_tooltip')) {
+    const tooltipText = individual['s-wf:SimpleRouteStartForm_tooltip'].map(veda.Util.formatValue).join(' ');
+    const tooltipTemplate = '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner aaas" style="text-align: left; white-space:break-spaces"></div></div>';
     const control = $('veda-control[rel="s-wf:SimpleRouteStartForm_participant"]', template);
     control.attr('data-tooltip', individual.tooltip);
     control.tooltip({
-      title: individual.tooltip,
-      placement: 'top',
+      title: tooltipText,
+      placement: 'bottom',
       container: container,
       animation: false,
+      template: tooltipTemplate,
+      sanitize: false
     });
   }
+  
+  template.on('validate', function () {
+    const result = {};
+    if (individual.hasValue('s-wf:SimpleRouteStartForm_participantMinCardinality') || individual.hasValue('s-wf:SimpleRouteStartForm_participantMinCardinality')) {
+      const currentParticipants = individual.hasValue('s-wf:SimpleRouteStartForm_participant') ? individual['s-wf:SimpleRouteStartForm_participant'].length : 0;
+      if (individual.hasValue('s-wf:SimpleRouteStartForm_participantMinCardinality')) {
+        if (currentParticipants < individual['s-wf:SimpleRouteStartForm_participantMinCardinality'][0]) {
+          result['s-wf:SimpleRouteStartForm_participant'] = {
+            state: false,
+            cause: ['v-ui:minCardinality'],
+          };
+        }
+      }
+      if (individual.hasValue('s-wf:SimpleRouteStartForm_participantMaxCardinality')) {
+        if (currentParticipants > individual['s-wf:SimpleRouteStartForm_participantMaxCardinality'][0]) {
+          result['s-wf:SimpleRouteStartForm_participant'] = {
+            state: false,
+            cause: ['v-ui:maxCardinality'],
+          };
+        }
+      }
+    } else {
+      result['s-wf:SimpleRouteStartForm_participant'] = {
+        state: true,
+        cause: ['v-ui:maxCardinality'],
+      };
+    }
+    template[0].dispatchEvent(new CustomEvent('validated', {detail: result}));
+  });
+
+  // if (individual.tooltip != undefined) {
+  //   const control = $('veda-control[rel="s-wf:SimpleRouteStartForm_participant"]', template);
+  //   control.attr('data-tooltip', individual.tooltip);
+  //   control.tooltip({
+  //     title: individual.tooltip,
+  //     placement: 'top',
+  //     container: container,
+  //     animation: false,
+  //   });
+  // }
 }
 
 export const html = `
