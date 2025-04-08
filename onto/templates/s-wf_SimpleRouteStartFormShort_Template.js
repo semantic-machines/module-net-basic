6940903b384eee6bfb34d553ba56fa5ddb85ecf4
lav <lapshin.a.v.395@gmail.com>
2025-04-08 16:41:46 +0300

    fix: update simple templates

--- onto/templates/s-wf_SimpleRouteStartFormShort_Template.js
+++ onto/templates/s-wf_SimpleRouteStartFormShort_Template.js
@@ -91,7 +91,7 @@ export const html = `
       </div>
       <div class="col-sm-3 col-xs-3">
         <div property="s-wf:SimpleRouteStartForm_deadlineDays" class="-view -edit search"></div>
-        <div about="@" property="s-wf:SimpleRouteStartForm_deadlineDays" class="view edit -search"></div>
+        <div about="@" property="s-wf:SimpleRouteStartForm_deadlineDays" class="view -edit -search"></div>
         <veda-control data-type="integer" property="s-wf:SimpleRouteStartForm_deadlineDays" class="-view edit -search"></veda-control>
       </div>
     </div>
